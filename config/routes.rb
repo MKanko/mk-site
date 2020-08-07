0a180a1f75c8e9e3b1a26fb5e10ecfc41dba5cd3
“Mark <mkanko66@gmail.com>
2020-08-07 10:53:01 -0700

    edit rails routes resources for home, and about, add componentDidMount to About.js, add mapStateToProps, and mapDispatchToProps to About.js, add segment, and edits to jsx in About.js

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  
-  resources :abouts
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
   
   resources :users, only: [:create, :show, :index]
-  resource :home
+  resource :home, except: [:index]
+  resources :about, except: [:index]
+  
 end
