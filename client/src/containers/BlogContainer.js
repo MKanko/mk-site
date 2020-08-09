b644f1fbeb7cd05b893bc5d2232d1f722f960f18
“Mark <mkanko66@gmail.com>
2020-08-09 15:06:18 -0700

    add edits to BlogContainer component (mapStateToProps, mapDispatchToProps, connect BlogContainer to Redux Store, componentDidMount, import getBlog action from src/actions, import PostIndex component, pass posts array as props to PostIndex component

--- client/src/containers/BlogContainer.js
+++ client/src/containers/BlogContainer.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Header, Grid } from 'semantic-ui-react'
+import { Grid, Header, Segment } from 'semantic-ui-react'
 
+import { getBlog } from '../actions'
+import PostIndex from '../components/post/PostIndex'
 
 class BlogContainer extends Component {
+
+    componentDidMount() {
+       this.props.getBlog() 
+    }
 
     render() {
         return (
             <div>
-                <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-                    <Grid.Column style={{ maxWidth: 450 }}>
-                        <Header as='h1' color='blue' textAlign='center'>
-                            Blog Page
-                        </Header>
-                    </Grid.Column>
+                <Grid container textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                    <Grid.Row columns={1}>
+                        <Grid.Column style={{ maxWidth: 450 }}>
+                            <Header as='h1' color='yellow' textAlign='center'>
+                                {this.props.title}
+                            </Header>
+                            <Segment inverted secondary>
+                                {this.props.text_content}
+                            </Segment>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <PostIndex posts={this.props.posts} />             
                 </Grid>
             </div>
         )
@@ -21,4 +33,18 @@ class BlogContainer extends Component {
 
 }
 
-export default connect(null)(BlogContainer)
+const mapStateToProps = (state) => {
+    console.log(state)
+    return {
+        title: state.manageBlog.blog.title,
+        text_content: state.manageBlog.blog.text_content,
+        image: state.manageBlog.blog.image,
+        posts: state.manageBlog.blog.posts
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {getBlog: () => {dispatch(getBlog())}}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BlogContainer)
