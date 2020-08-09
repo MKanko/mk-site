cf2bc3fc87c526d2dffaad820816218c0f985607
“Mark <mkanko66@gmail.com>
2020-08-09 14:49:45 -0700

    add has_many macro to Blog model for has_many/belongs_to association on the backend - Blog has_many posts and Post belongs_to Blog, belongs_to macro added to Post model when Post resource was generated, build out blogs controller, build out posts controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,40 @@
 class BlogsController < ApplicationController
+
+    def new 
+        blog = Blog.new 
+    end 
+
+    def create 
+        blog = Blog.create(blog_params)
+        render json: BlogSerializer.new(blog)
+    end 
+
+    def show 
+        blog = Blog.first 
+        render json: BlogSerializer.new(blog)
+    end 
+
+    def edit 
+        blog = Blog.first 
+        render json: BlogSerializer.new(blog)
+    end 
+
+    def update
+        blog = Blog.first 
+        blog.update(blog_params)
+        blog.save 
+        render json: BlogSerializer.new(blog)
+    end 
+
+    def destroy
+        blog = blog.first
+        blog.destroy 
+    end 
+
+    private 
+
+    def blog_params 
+        params.require(:blog).permit(:title, :text_content, :image)
+    end 
+
 end
