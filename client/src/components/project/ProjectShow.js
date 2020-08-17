8bbeadcf3b6bad97e8120c2a2a7dfbccd953f308
“Mark <mkanko66@gmail.com>
2020-08-17 15:24:22 -0700

    Refactor ProjectShow Component
    - refactor ProjectShow to debug lost history when link to project url
    - add edits to refactor ProjectShow from functional component to class based connected to redux store

--- client/src/components/project/ProjectShow.js
+++ client/src/components/project/ProjectShow.js
@@ -1,34 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { getProject } from '../actions'
 import {Grid, Header, Segment, Transition, List} from 'semantic-ui-react'
 
-const ProjectShow = (props) => {
-console.log('show props:', props)
+class ProjectShow extends Component {
 
-    const techDetailList = props.location.query.project.technical_details.map(techDetail => <List.Item>{techDetail}</List.Item>)
+    techDetailList = () => {
+        return this.props.project.technical_details.map(techDetail => <List.Item>{techDetail}</List.Item>)
+    } 
 
-    return (
-        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-            <Transition animation={'drop'} duration={2000} transitionOnMount>
-                <Grid.Column style={{ maxWidth: 600 }}>
-                    <Header as='h1' color='green' textAlign='center'>
-                        {props.location.query.project.name}
-                    </Header>
-                    <Segment inverted secondary>
-                        {props.location.query.project.description}
-                    </Segment>
-                    <Segment inverted secondary>
-                        <List>
-                            {techDetailList}
-                        </List>                           
-                    </Segment>
-                    <Segment inverted secondary>
-                        <a href={props.location.query.project.project_link}>View Site</a>
-                    </Segment>
-                </Grid.Column>
-            </Transition>
-            </Grid>      
-    )
+    render() {
+        return (
+            <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                <Transition animation={'drop'} duration={2000} transitionOnMount>
+                    <Grid.Column style={{ maxWidth: 600 }}>
+                        <Header as='h1' color='green' textAlign='center'>
+                            {this.props.project.name}
+                        </Header>
+                        <Segment inverted secondary>
+                            {this.props.project.description}
+                        </Segment>
+                        <Segment inverted secondary>
+                            <List>
+                                {techDetailList()}
+                            </List>                           
+                        </Segment>
+                        <Segment inverted secondary>
+                            <a href={this.props.project.project_link}>View Site</a>
+                        </Segment>
+                    </Grid.Column>
+                </Transition>
+                </Grid>      
+        )
+    }
 
 }
 
-export default ProjectShow 
+const mapStateToProps = (state) => {
+    return {
+        name: state.manageProjects.project.name,
+        description: state.manageProjects.project.description,
+        image: state.manageProjects.project.image,
+        technical_details: state.manageProjects.project.technical_details,
+        project_link: state.manageProjects.project.project_link 
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {getProject: (id) => {dispatch(getProject(id))}}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectShow) 
