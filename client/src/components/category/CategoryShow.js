e8b4ac78e80d270233706ad29385a8cfa2d2031c
“Mark <mkanko66@gmail.com>
2020-08-18 10:31:21 -0700

    Add CategoryShow
    - add CategoryShow.js to components/category directory
    - build out CategoryShow component

---
+++ client/src/components/category/CategoryShow.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { getCategory } from '../../actions'
+
+
+class CategoryShow extends Component {
+
+    componentDidMount() {
+        this.props.getCategory(this.props.location.pathname.replace('/categories/', ''))
+    }
+
+    render() {
+        return (
+            <div>          
+                <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                    <Transition animation={'drop'} duration={2000} transitionOnMount>
+                        <Grid.Column style={{ maxWidth: 600 }}>
+                            <Header as='h1' color='green' textAlign='center'>
+                                {this.props.title}
+                            </Header>
+                            <Segment inverted secondary>
+                                {this.props.text_content}
+                            </Segment>
+                        </Grid.Column>
+                    </Transition>
+                </Grid>
+            </div>
+        )
+    }
+
+}
+
+const mapStateToProps = (state) => {
+    return {
+        title: state.manageCategories.category.title,
+        text_content: state.manageCategories.category.text_content,
+        image: state.manageCategories.category.image
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {getCategory: (id) => {dispatch(getCategory(id))}}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryShow)
