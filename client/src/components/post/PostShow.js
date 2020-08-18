f1dc966cefeb010eb2e6425764340ea72b67364b
“Mark <mkanko66@gmail.com>
2020-08-17 18:05:28 -0700

    Refactor PostShow and PostDetail Components
    - refactor components to solve loss of state
    - add edits to PostShow to make it a class based component connected to redux store
    - add import for getProject action creator and add mapStateToProps and mapDispatchToProps
    - add edits to PostDetail, eliminate query object in link which was passing post attributes to PostShow
    - misc edits other files

--- client/src/components/post/PostShow.js
+++ client/src/components/post/PostShow.js
@@ -1,26 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react'
 import {Grid, Header, Segment, Transition} from 'semantic-ui-react'
 
-const PostShow = (props) => {
-console.log('show props:', props)
-    return (
-        <div>
-            <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-                <Transition animation={'drop'} duration={2000} transitionOnMount>
-                    <Grid.Column style={{ maxWidth: 600 }}>
-                        <Header as='h1' color='green' textAlign='center'>
-                            {props.location.query.post.title}
-                        </Header>
-                        <Segment inverted secondary>
-                            {props.location.query.post.text_content}
-                        </Segment>
-                    </Grid.Column>
-                </Transition>
-                </Grid>
-        </div>
-    )
+import { getPost } from '../../actions'
+
+class PostShow extends Component {
+
+    render() {
+        return (
+            <div>
+                <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                    <Transition animation={'drop'} duration={2000} transitionOnMount>
+                        <Grid.Column style={{ maxWidth: 600 }}>
+                            <Header as='h1' color='green' textAlign='center'>
+                                {this.props.title}
+                            </Header>
+                            <Segment inverted secondary>
+                                {this.props.text_content}
+                            </Segment>
+                        </Grid.Column>
+                    </Transition>
+                    </Grid>
+            </div>
+        )
+    }
+
 
 }
 
-export default PostShow
+const mapStateToProps = (state) => {
+    return {
+        title: state.managePosts.post.title,
+        text_content: state.managePosts.post.text_content,
+        image: state.managePosts.post.image
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {getPost: (id) => {dispatch(getPost(id))}}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostShow)
 
