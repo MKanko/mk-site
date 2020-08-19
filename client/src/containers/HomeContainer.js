3e68d79355dc5cdaeaefc3cffedc3ff28ede87c4
“Mark <mkanko66@gmail.com>
2020-08-18 17:54:12 -0700

    Add HomeContainer
    - add HomeContainer file to containers directory
    - stub our HomeContainer component
    - minor change in manageProjects reducer

---
+++ client/src/containers/HomeContainer.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { getHome } from '../actions'
+
+class HomeContainer extends Component {
+
+    componentDidMount() {
+        this.props.getHome()
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+
+}
+
+const mapStateToProps = (state) => {
+    return {home: state.manageHome.home}
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {getHome: () => {dispatch(getHome())}}
+} 
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeContainer)
