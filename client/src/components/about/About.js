5e7a2d1f108855afcd2c918aba7af5bc8658b6a2
“Mark <mkanko66@gmail.com>
2020-08-18 20:13:25 -0700

    Add Edits For AboutDetail
    - build out AboutDetail component
    - add edits to import AboutContainer in App.js
    - temp comment out code in original About component

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,58 +1,58 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Header, Grid, Segment, Image } from 'semantic-ui-react'
+// import React, { Component } from 'react'
+// import { connect } from 'react-redux'
+// import { Header, Grid, Segment, Image } from 'semantic-ui-react'
 
-import { getAbout, getSkills } from '../../actions'
-import SkillIndex from '../skill/SkillIndex'
+// import { getAbout, getSkills } from '../../actions'
+// import SkillIndex from '../skill/SkillIndex'
 
-class About extends Component {
+// class About extends Component {
 
-    componentDidMount() {
-        this.props.getAbout()
-        this.props.getSkills()
-    }
+//     componentDidMount() {
+//         this.props.getAbout()
+//         this.props.getSkills()
+//     }
 
-    render() {
-        return (
-            <Grid container textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-                <Grid.Row>
-                    <Grid.Column style={{ maxWidth: 450 }}>
-                        <Header as='h1' style={{color: '#0E15FA'}} textAlign='center'>
-                            {this.props.title}
-                        </Header>
-                        {/* <Card centered style={{backgroundColor: 'grey' }}> */}
-                            <Image src={process.env.PUBLIC_URL + this.props.image} />
-                        {/* </Card> */}
-                        <Segment inverted secondary>
-                            {this.props.text_content}
-                        </Segment>
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>                    
-                    {/* <Header as='h1' style={{color: '#0E15FA'}} textAlign='center'>Skills</Header>                       */}
-                    <SkillIndex getSkills={this.props.getSkills} skills={this.props.skills} />                                                          
-                </Grid.Row>               
-            </Grid>
-        )
-    }
+//     render() {
+//         return (
+//             <Grid container textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+//                 <Grid.Row>
+//                     <Grid.Column style={{ maxWidth: 450 }}>
+//                         <Header as='h1' style={{color: '#0E15FA'}} textAlign='center'>
+//                             {this.props.title}
+//                         </Header>
+//                         {/* <Card centered style={{backgroundColor: 'grey' }}> */}
+//                             <Image src={process.env.PUBLIC_URL + this.props.image} />
+//                         {/* </Card> */}
+//                         <Segment inverted secondary>
+//                             {this.props.text_content}
+//                         </Segment>
+//                     </Grid.Column>
+//                 </Grid.Row>
+//                 <Grid.Row>                    
+//                     {/* <Header as='h1' style={{color: '#0E15FA'}} textAlign='center'>Skills</Header>                       */}
+//                     <SkillIndex getSkills={this.props.getSkills} skills={this.props.skills} />                                                          
+//                 </Grid.Row>               
+//             </Grid>
+//         )
+//     }
 
-}
+// }
 
-const mapStateToProps = (state) => {
-    console.log('state:', state)
-    return {
-        title: state.manageAbout.about.title,
-        text_content: state.manageAbout.about.text_content,
-        image: state.manageAbout.about.image, 
-        skills: state.manageSkills.skills
-    }
-}
+// const mapStateToProps = (state) => {
+//     console.log('state:', state)
+//     return {
+//         title: state.manageAbout.about.title,
+//         text_content: state.manageAbout.about.text_content,
+//         image: state.manageAbout.about.image, 
+//         skills: state.manageSkills.skills
+//     }
+// }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        getAbout: () => {dispatch(getAbout())},
-        getSkills: () => {dispatch(getSkills())}
-    }
-}
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         getAbout: () => {dispatch(getAbout())},
+//         getSkills: () => {dispatch(getSkills())}
+//     }
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(About)
+// export default connect(mapStateToProps, mapDispatchToProps)(About)
---
+++ client/src/components/about/AboutDetail.js
@@ -0,0 +1,30 @@
+import React from 'react' 
+import { Grid, Header, Image, Segment } from 'semantic-ui-react'
+
+const AboutDetail = (props) => {
+
+    return (
+        <Grid container textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+            <Grid.Row>
+                <Grid.Column style={{ maxWidth: 450 }}>
+                    <Header as='h1' style={{color: '#0E15FA'}} textAlign='center'>
+                        {props.about.title}
+                    </Header>
+                    {/* <Card centered style={{backgroundColor: 'grey' }}> */}
+                        <Image src={process.env.PUBLIC_URL + props.about.image} />
+                    {/* </Card> */}
+                    <Segment inverted secondary>
+                        {props.about.text_content}
+                    </Segment>
+                </Grid.Column>about
+            </Grid.Row>
+            <Grid.Row>                    
+                {/* <Header as='h1' style={{color: '#0E15FA'}} textAlign='center'>Skills</Header>                       */}
+                <SkillIndex getSkills={props.skills.getSkills} skills={props.skills.skills} />                                                          
+            </Grid.Row>               
+        </Grid> 
+    )
+
+}
+
+export default AboutDetail
