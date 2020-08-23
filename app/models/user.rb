c84ac4fe729e5e2817edce9521825922bb7f80c4
“Mark <mkanko66@gmail.com>
2020-08-23 14:40:43 -0700

    Initial commit
    - generate migration to add column admin_secret to users table in db
    - run migration
    - add validation confirm_admin_secret and confirm_admin_secret method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
 
     validates :username, presence: true
     validates :username, uniqueness: true
+
+    validate :confirm_admin_secret
+
+    def confirm_admin_secret
+        if self.confirm_admin != ENV['ADMIN_SECRET']
+            errors.add(:admin, "Admin secret is incorrect.")
+        end          
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_171225) do
+ActiveRecord::Schema.define(version: 2020_08_23_212525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,6 +121,7 @@ ActiveRecord::Schema.define(version: 2020_08_19_171225) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "admin_secret"
   end
 
   add_foreign_key "categories", "resumes"
