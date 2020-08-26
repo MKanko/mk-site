1e18cff9f57f237cb899d6017053c4814fb9439a
“Mark <mkanko66@gmail.com>
2020-08-25 20:25:01 -0700

    Cleanup Naming Multiple Files
    - add edits to clean up dyslexic naming of files and paths multiple files

--- client/src/App.js
+++ client/src/App.js
@@ -10,26 +10,26 @@ import About from './containers/AboutContainer'
 import PortfolioContainer from './containers/PortfolioContainer'
 import BlogContainer from './containers/BlogContainer'
 import ResumeContainer from './containers/ResumeContainer'
-import EditHome from './components/admin/home/EditHome'
-import EditAbout from './components/admin/about/EditAbout'
-import EditPortfolio from './components/admin/portfolio/EditPortfolio'
+import EditHome from './components/admin/home/HomeEdit'
+import EditAbout from './components/admin/about/AboutEdit'
+import EditPortfolio from './components/admin/portfolio/PortfolioEdit'
 import ProjectNew from './components/admin/portfolio/project/ProjectNew'
-import EditProject from './components/admin/portfolio/project/EditProject'
+import EditProject from './components/admin/portfolio/project/ProjectEdit'
 import ProjectShow from './components/project/ProjectShow'
 import PostShow from './components/post/PostShow'
-import EditBlog from './components/admin/blog/EditBlog'
+import EditBlog from './components/admin/blog/BlogEdit'
 import PostNew from './components/admin/blog/post/PostNew'
-import EditPost from './components/admin/blog/post/EditPost'
-import EditResume from './components/admin/resume/EditResume'
+import EditPost from './components/admin/blog/post/PostEdit'
+import EditResume from './components/admin/resume/ResumeEdit'
 import Contact from './containers/ContactContainer'
-import EditContact from './components/admin/contact/EditContact'
+import EditContact from './components/admin/contact/ContactEdit'
 import PageEffect from './components/transition/PageEffect'
 import EmailNew from './components/email/EmailNew'
 import SkillIndex from './components/skill/SkillIndex'
 import SkillNew from './components/admin/skill/SkillNew'
-import EditSkill from './components/admin/skill/EditSkill'
+import EditSkill from './components/admin/skill/SkillEdit'
 import CategoryNew from './components/admin/resume/category/CategoryNew'
-import EditCategory from './components/admin/resume/category/EditCategory'
+import EditCategory from './components/admin/resume/category/CategoryEdit'
 import CategoryShow from './components/category/CategoryShow'
 
 class App extends Component { 
@@ -55,18 +55,18 @@ class App extends Component {
                         <Route exact path='/home/edit' component={EditHome} />
                         <Route exact path='/about/edit' component={EditAbout} />
                         <Route exact path='/portfolio/edit' component={EditPortfolio} />
-                        <Route exact path='/projects/create' component={ProjectNew} />
+                        <Route exact path='/projects/new' component={ProjectNew} />
                         <Route exact path='/project/edit' component={EditProject} />
                         <Route exact path='/blog/edit' component={EditBlog} />
-                        <Route exact path='/posts/create' component={PostNew} />
+                        <Route exact path='/posts/new' component={PostNew} />
                         <Route exact path='/post/edit' component={EditPost} />
                         <Route exact path='/resume/edit' component={EditResume} />
-                        <Route exact path='/categories/create' component={CategoryNew} />
+                        <Route exact path='/categories/new' component={CategoryNew} />
                         <Route exact path='/category/edit' component={EditCategory}/>
                         <Route exact path='/contact/edit' component={EditContact} />
                         <Route exact path='/emails' component={EmailNew} />
                         <Route exact path='/skills' component={SkillIndex} />
-                        <Route exact path='/skills/create' component={SkillNew} />
+                        <Route exact path='/skills/new' component={SkillNew} />
                         <Route exact path='/skill/edit' component={EditSkill} />
 
                         <Route exact path='/' render={(props) => (
