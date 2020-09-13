89da38f57333ae979672e292b628f58981fd270f
“Mark <mkanko66@gmail.com>
2020-09-13 15:44:04 -0700

    edit backend.js to fetch from https instead of http

--- client/src/apis/backend.js
+++ client/src/apis/backend.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const URL = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 'http://localhost:3001' : 'http://api.markkanko.com'
+const URL = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 'http://localhost:3001' : 'https://api.markkanko.com'
 
 
 export default axios.create({baseURL: URL})
