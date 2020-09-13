2d6d2f522669b21edd4175c2e65c77cda5e8893f
“Mark <mkanko66@gmail.com>
2020-09-13 12:23:29 -0700

    Category Page Changes
    - add Cat.css file with media query css for responsive mobile display
    - add changes in CatDetail for resposive mobile display
    - delete commented out unecessary code in Post.css and Project.css

--- client/src/components/category/CategoryDetail.js
+++ client/src/components/category/CategoryDetail.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Grid, Card, Image } from 'semantic-ui-react'
+import './Category.css'
 
 const CategoryDetail = (props) => {
 
     return (
-        <Grid.Column width={5}>
-            <Card centered as={Link} to={{pathname: '/categories/' + props.category.id}} style={{backgroundColor: 'grey'}}>
-                <Image src={process.env.PUBLIC_URL + props.category.image} size='medium' style={{objectFit: 'cover', height: '12em'}} />
-                <Card.Content>
-                    <Card.Header>{props.category.title}</Card.Header>
-                    {/* <Card.Description>{props.category.text_content}</Card.Description> */}
-                </Card.Content>
-            </Card>
-        </Grid.Column>
+        <>
+            <Grid.Column id="desktop" width={5}>
+                <Card centered as={Link} to={{pathname: '/categories/' + props.category.id}} style={{backgroundColor: 'grey'}}>
+                    <Image src={process.env.PUBLIC_URL + props.category.image} size='medium' style={{objectFit: 'cover', height: '12em'}} />
+                    <Card.Content>
+                        <Card.Header>{props.category.title}</Card.Header>
+                        {/* <Card.Description>{props.category.text_content}</Card.Description> */}
+                    </Card.Content>
+                </Card>
+            </Grid.Column>
+            <Grid.Column id="mobile" mobile={10}>
+                <Card centered as={Link} to={{pathname: '/categories/' + props.category.id}} style={{backgroundColor: 'grey'}}>
+                    <Image src={process.env.PUBLIC_URL + props.category.image} size='medium' style={{objectFit: 'cover', height: '12em'}} />
+                    <Card.Content>
+                        <Card.Header>{props.category.title}</Card.Header>
+                        {/* <Card.Description>{props.category.text_content}</Card.Description> */}
+                    </Card.Content>
+                </Card>
+            </Grid.Column>
+        </>
     )
 }
 
